02038c535de5d44fadfe5596183cf11b1c328820
leeyan821 <mu1521@naver.com>
2023-06-10 20:06:45 +0900

    ?

--- src/view/Main.java
+++ src/view/Main.java
@@ -4,14 +4,11 @@ import controller.movie.MovieController;
 import dto.movie.MoviList;
 
 import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Vector;
-
 
 public class Main extends JFrame {
     private JPanel mPanel;
